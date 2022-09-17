f32cbb5ea8a5f3967f750826f605c0dbca6048fb
Tang Thu Hoa <thuhoatang2001@gmail.com>
2022-09-17 15:05:45 +0700

    Bai tap 4

---
+++ bt4.rb
@@ -0,0 +1,15 @@
+arr = []
+print("Nhap so nguyen thu 1 : ")
+arr.append( gets.chomp)
+print("Nhap so nguyen thu 2 : ")
+arr.append( gets.chomp)
+
+print("Nhap so nguyen thu 3 : ")
+arr.append( gets.chomp)
+
+
+
+print("Mảng Giảm dần : ", arr.sort().reverse(),"\n" )
+
+print("Mảng Tăng dần : ", arr.sort() )
+
